ba20834999d8b4550436a173d78711c39ce735bb
mark sauer-utley <mark.utley.713@gmail.com>
2018-11-01 15:27:42 -0400

    add gitignore and mapquest api

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const key = process.env.KEY;
+const secret = process.env.SECRET;
+
+const location = "109 Joralemon St Brooklyn";
+const locationUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=${key}&location=${location}`
+
 const url = `https://api.darksky.net/forecast/${process.env.SECRET}/40.6932099,-73.9953689`
 const requestObj = { url, json: true };
+
+const locationObj = { url: locationUrl, json: true };
 
 const requestCB = (err, resp, body) => {
   if (err) {
@@ -11,4 +21,4 @@ const requestCB = (err, resp, body) => {
   console.log(JSON.stringify(body, undefined, 2));
 }
 
-request(requestObj, requestCB);
+request(locationObj, requestCB);
