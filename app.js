7b96eb543f75f4e80a43ae0b4d66dd75764dd24a
mark sauer-utley <mark.utley.713@gmail.com>
2018-11-02 12:42:59 -0400

    give geocode a callback function

--- app.js
+++ app.js
@@ -12,7 +12,10 @@ const yargOptions = {
 }
 
 const argv = yargs.options(yargOptions).help().argv;
-geocode.geocodeAddress(argv.address);
+
+geocode.geocodeAddress(argv.address, (errorMessage, results) => {
+  errorMessage ? console.log(errorMessage) : console.log(JSON.stringify(results, undefined, 2));
+});
 
 
 
--- geocode/geocode.js
+++ geocode/geocode.js
@@ -1,28 +1,30 @@
 const request = require('request');
 const key = process.env.KEY;
 
-const geocodeAddress = (address) => {
+const geocodeAddress = (address, callback) => {
   const encoded = encodeURIComponent(address);
   const locationUrl = `http://www.mapquestapi.com/geocoding/v1/address?key=${key}&location=${encoded}`;
   const locationObj = { url: locationUrl, json: true };
 
   request(locationObj, (err, resp, body) => {
     if (!body.results || err) {
-      console.log(`Error: unable to connect to mapquest servers`);
+      callback(`Error: unable to connect to mapquest servers`);
+    } else if (!body.results[0].locations[0]) {
+      callback(`Error: unable to find address`);
     } else if (!body.results[0].locations[0].latLng) {
-      console.log(`Error: unable to find latitude and longitude`);
+      callback(`Error: unable to find latitude and longitude`);
     } else {
       const { locations } = body.results[0];
       const { street, latLng } = locations[0];
 
-      console.log(JSON.stringify(`Address: ${street !== "" ? street : "not found"}`, undefined, 2));
-      console.log(JSON.stringify(`Latitude: ${latLng.lat}`, undefined, 2));
-      console.log(JSON.stringify(`Longitude: ${latLng.lng}`, undefined, 2));
+       callback(undefined, {
+        address: (street !== "" ? street : "not found"),
+        latitude: latLng.lat,
+        longitude: latLng.lng,
+      })
     }
 
   });
 }
 
-module.exports = {
-  geocodeAddress
-}
+module.exports = { geocodeAddress };
